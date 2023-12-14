919021ddbb5ea6bf06e48401fbce30ad37494bff
TubiOb <Tubiobaloluwa@gmail.com>
2023-12-14 07:37:47 +0100

    Corrected company name

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -9,11 +9,20 @@ import L from '../assets/Vector-l.svg'
 import O from '../assets/Vector-o.svg'
 import U from '../assets/Vector-u.svg'
 import D from '../assets/Vector-d.svg'
+import E from '../assets/Vector-e.svg'
+import Q from '../assets/Vector-q.svg'
+import U2 from '../assets/Vector-u2.svg'
+import I from '../assets/Vector-i.svg'
+import P from '../assets/Vector-p.svg'
+import M from '../assets/Vector-m.svg'
+import E2 from '../assets/Vector-e2.svg'
+import N from '../assets/Vector-n.svg'
+import T from '../assets/Vector-t.svg'
 
 
 const Navigation = () => {
   const location = useLocation();
-  const [activeLi, setActiveLi] = useState('Products');
+  // const [activeLi, setActiveLi] = useState('Products');
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
@@ -22,7 +31,7 @@ const Navigation = () => {
 
   const handleClick = (clickedLink) => {
       // console.log(`Clicked on: ${clickedLink}`);
-      setActiveLi(clickedLink);
+      // setActiveLi(clickedLink);
       setIsMenuOpen(false);
   };
 
@@ -39,16 +48,11 @@ const Navigation = () => {
   return (
     <div className={`fixed bg-white z-40 flex xl:flex-row lg:flex-row 2xl:flex-row md:flex-col flex-col items-start md:items-start lg:items-center xl:items-center 2xl:items-center justify-between md:right-0 lg:w-full xl:w-full 2xl:w-full w-[70%] md:w-[50%] lg:h-14 xl:h-14 2xl:h-16 h-full md:h-full lg:py-1 px-5 py-10 xl:py-4 xl:px-8 gap-9 lg:gap-1 ${isMenuOpen ? 'right-0' : 'right-[-100%]'} `}>
       <div className='w-16 h-6 cursor-pointer flex flex-row gap-2'>
-          <Link to='/product' className='w-full h-full cursor-pointer'>
+          <Link to='/product' className='w-full h-full cursor-pointer flex flex-row items-center justify-between gap-2'>
               <img src={logo} alt="Golio" className='w-full h-full' />
+              <h2 className="flex text-neutral-500 font-['Manrope'] font-bold text-lg tracking-wide">Cloud<span className='font-normal'>Equipment</span></h2>
           </Link>
-          <div className='flex flex-row items-center h-7 justify-between'>
-            <img src={C} alt="" />
-            <img src={L} alt="" />
-            <img src={O} alt="" />
-            <img src={U} alt="" />
-            <img src={D} alt="" />
-          </div>
+          
       </div>
 
             
@@ -58,7 +62,7 @@ const Navigation = () => {
       <ul className='flex h-[50%] lg:h-full lg:flex-row flex-col lg:items-center xl:items-center items-start justify-between lg:py-3 list-none gap-0.5 md:gap-1 lg:gap-2 text-sm 2xl:text-lg py-8 lg:px-1 px-0.5'>
           {links.map((link) => (
             <Link to={link.link} key={link.name}>
-              <li className={`transition-all cursor-pointer py-2 px-4 duration-300 ${activeLi === link.name ? ' text-[#54D4BD] md:hover:bg-neutral-200 lg:hover:bg-transparent lg:hover:rounded-none hover:rounded-md' : ' text-neutral-500 hover:text-[#54D4BD] '}`}  onClick={() => handleClick(link.name)}>
+              <li className={`transition-all cursor-pointer py-2 px-4 duration-300 ${location.pathname === link.link ? ' text-[#54D4BD] md:hover:bg-neutral-200 lg:hover:bg-transparent lg:hover:rounded-none hover:rounded-md' : ' text-neutral-500 hover:text-[#54D4BD] '}`}  onClick={() => handleClick(link.name)}>
                   {link.name}
               </li>
             </Link>
@@ -80,7 +84,7 @@ const Navigation = () => {
                 ?
                 (<LiaWindowCloseSolid  size={30} className='text-black z-50 fixed' />)
                 :
-                (<HiMenuAlt3 size={30} className='text-black z-50 fixed' />)
+                (<HiMenuAlt3 size={30} className='text-white z-50 fixed bg-gradient-to-r from-blue-300 via-black rounded-lg to-green-500' />)
             }
       </div>
     </div>
