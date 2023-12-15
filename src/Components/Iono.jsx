e5900697b6b5c897345232f522de11d183dcdcfe
TubiOb <Tubiobaloluwa@gmail.com>
2023-12-15 12:55:47 +0100

    Corrected Distorted spacing

--- src/Components/Iono.jsx
+++ src/Components/Iono.jsx
@@ -25,7 +25,7 @@ const Iono = () => {
         <img className='relative object-cover w-full h-[500px] md:h-auto' src={photo} alt='A captivating visual' />
         <img alt="" className='z-10 w-[15%] h-[15%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute top-80 sm:top-60 md:top-1/2' src={playButton} />
         
-        <div className="flex text-center flex-shrink gap-5 md:gap-7 lg:gap-10 justify-center text-white absolute
+        <div className="flex text-center flex-shrink gap-5 md:gap-7 lg:gap-52 justify-center text-white absolute
          z-20 bg-[#42526B] items-center w-[95%] md:w-[80%] lg:w-[80%] mx-auto lg:bottom-[-160px] xl:bottom-[-120px] 2xl:bottom-[-100px] left-0 right-0 bottom-[-50px] px-2 lg:px-5 py-3 lg:py-10 xl:py-10 2xl:py-10">
           {boxesContent.map((box, index) => (
             <div className="flex-col" key={index}>
