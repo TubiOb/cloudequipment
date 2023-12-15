f7d06942b5219e577edaa90d6c29909dc7ae0425
TubiOb <Tubiobaloluwa@gmail.com>
2023-12-15 11:05:32 +0100

    C'est fini

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
                     <h4 className="text-[#70FFE5] font-bold tracking-[7px] leading-6 font-['Roboto'] roboto text-sm md:text-[14px] xl:text-lg 2xl:text-2xl">INVESTORS</h4>
                     <h1 className="2xl:text-5xl xl:text-3xl md:text-2xl text-xl font-medium leading-9 2xl:leading-[55px] xl:leading-[42px] md:leading-[30px] font-['Roboto'] roboto drop-shadow-2xl">Invest in Medical equipment for African Healthcare and get ROI</h1>
                     <p className='text-xs md:text-sm lg:text-base xl:text-lg 2xl:text-2xl font-normal leading-5 xl:leading-7 manrope 2xl:tracking-wider 2xl:leading-[50px] py-2 drop-shadow-2xl'>We provide African healthcare facilities with cutting-edge medical equipment to provide the best quality of care to their patients - with financing, maintenance and support.</p>
-                    <div className='flex items-start justify-between 2xl:w-[70%] xl:w-[65%] md:w-[55%] w-[90%] md:gap-1 xl:gap-2.5 2xl:gap-4 mt-2 2xl:mt-4'>
+                    <div className='flex items-start justify-between 2xl:w-[70%] xl:w-[65%] md:w-[55%] w-[70%] md:gap-1 xl:gap-2.5 2xl:gap-4 mt-2 2xl:mt-4'>
                         <button className="bg-[#70FFE5] tracking-wider cursor-pointer text-black font-['Manrope'] manrope rounded-lg font-extrabold py-1.5 2xl:py-3 px-3 md:px-4 xl:px-4 2xl:px-14 items-center">
                             <Link to='/' className='text-xs md:text-sm lg:text-sm xl:text-base 2xl:text-lg'>
                             Get Started
